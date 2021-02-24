1158b590d167149abf1119c83b1bbf94305fb0d9
markdawson <mark@markdawson.io>
2021-02-24 14:47:05 -0800

    got two balls

--- main.cpp
+++ main.cpp
@@ -1,85 +1,88 @@
+#include "rtweekend.h"
+
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <string>
+#include <cmath>
 
-double hit_sphere(const point3 &center, double radius, const ray &r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant)) /  a;
-    }
-}
 
-color ray_color(const ray &r, const vec3 &normal_vec) {
-    auto t = hit_sphere(point3(0, 0, -1.0), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - normal_vec); //vec3(0, 0, -0.4));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray &r, const hittable &world) {
+    hit_record rec;
+
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-
-int main() {
-
+void render_image(std::string const& file_name, double i, int width) {
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 800;
+    const int image_width = width;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100 + i, -1), 100));
 
     // Camera
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
     auto focal_length = 1.0;
-
     auto origin = point3(0, 0, 0);
     auto horizontal = vec3(viewport_width, 0, 0);
     auto vertical = vec3(0, viewport_height, 0);
     auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
-    // Animation
+    // Opening file
+    std::filebuf fb;
+    fb.open(file_name, std::ios::out);
+    std::ostream os(&fb);
 
-    int image_num = -1;
-    float val;
-    for (int ival = -3000; ival <= -2000; ival++) {
-        val = ival / 5'000.0;
-        image_num += 1;
-        // Open file
-        std::filebuf fb;
-        char file_name [100];
-        std::snprintf(file_name, 100, "../images/image%04d.ppm", image_num);
-        fb.open(file_name, std::ios::out);
-        std::ostream os(&fb);
+    // std::cerr << "\rWriting file: " << ' ' << std::flush;
 
-        std::cerr << "\rWriting file: " << image_num  << ' ' << std::flush;
+    // Render
+    os << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-        // Render
-        os << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-        for (int j = image_height - 1; j >= 0; --j) {
-            // std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-            for (int i = 0; i < image_width; ++i) {
-                auto u = double(i) / (image_width - 1);
-                auto v = double(j) / (image_height - 1);
-                ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-                color pixel_color = ray_color(r, vec3(0, 0, val));
-                write_color(os, pixel_color);
-            }
+    for (int j = image_height - 1; j >= 0; --j) {
+        // std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            color pixel_color = ray_color(r, world);
+            write_color(os, pixel_color);
         }
-
-        fb.close();
-        // std::cerr << "\nDone.\n";
     }
+
+    fb.close();
+    // std::cerr << "\nDone.\n";
+}
+
+int main() {
+
+    // Open file
+    int image_num = -1;
+    for(int ival = -35; ival < 5; ival++) {
+        image_num++;
+
+        std::cerr << "\rWriting file: " << image_num << std::flush;
+
+        std::filebuf fb;
+        char file_name[100];
+        std::snprintf(file_name, 100, "../images/image%04d.ppm", image_num);
+
+
+        render_image(file_name, std::min(ival / 40.0, -0.125), 1'500);
+    }
+
+//      int width = 1'500;
+//      render_image("../images/image1.ppm", -0.5, width);
 }
